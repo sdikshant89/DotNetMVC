eb21d89e80614546129357f9d7161c1b7cfb43b2
sdikshant89 <sdikshant89@gmail.com>
2025-03-21 21:53:24 -0600

    Added new bootstrap theme and created table to list categories.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetMVC.Data;
+using DotNetMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace DotNetMVC.Controllers
 {
+
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 using DotNetMVC.Models;
 using Microsoft.EntityFrameworkCore;
 namespace DotNetMVC.Data
@@ -6,8 +7,20 @@ namespace DotNetMVC.Data
 	public class ApplicationDbContext : DbContext
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-		{}
+		{
+
+		}
 
 		public DbSet<Category> Categories { get; set; }
-	}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			modelBuilder.Entity<Category>().HasData(
+					new Category { CategoryId= 1, Name = "Action", DisplayOrder = 1 },
+                    new Category { CategoryId = 2, Name = "Romantic", DisplayOrder = 2 },
+					new Category { CategoryId = 3, Name = "Kids", DisplayOrder = 3 }
+				);
+        }
+
+    }
 }
